7fb0f00a17e6b6ead9b33c9e18fb83df118698a6
Ponomareva Nina <nina.9412@mail.ru>
2023-03-14 16:34:06 +0300

    feat: add linter configurations for the project

--- youtube_client_app/src/app/app.module.ts
+++ youtube_client_app/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import AppRoutingModule from './app-routing.module';
+import AppComponent from './app.component';
+import HeaderComponent from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export default class AppModule {}
