4b99c9577ba89748a6048aa2c5e0dfaf584b0094
Ponomareva Nina <nina.9412@mail.ru>
2023-03-15 19:14:02 +0300

    feat: add basic project structure

--- youtube_client_app/src/app/app.module.ts
+++ youtube_client_app/src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import HeaderComponent from './header/header.component';
+import ProfileComponent from './header/profile/profile.component';
+import SearchListComponent from './search/search-list/search-list.component';
+import SearchItemComponent from './search/search-item/search-item.component';
+import { SearchPanelComponent } from './header/search-panel/search-panel.component';
+import { SortPanelComponent } from './header/sort-panel/sort-panel.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    SearchListComponent,
+    SearchItemComponent,
+    SearchPanelComponent,
+    SortPanelComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ youtube_client_app/src/app/header/sort-panel/sort-panel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-panel',
+  templateUrl: './sort-panel.component.html',
+  styleUrls: ['./sort-panel.component.scss'],
+})
+export class SortPanelComponent {}
