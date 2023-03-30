9bad58e7f6bc9006a76b337de4a585411c00882f
Ponomareva Nina <nina.9412@mail.ru>
2023-03-30 17:47:39 +0300

    feat: add adaptive dtyles for header

--- youtube_client_app/src/app/core/header/profile/profile.component.ts
+++ youtube_client_app/src/app/core/header/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export default class ProfileComponent {
-  public name = 'Your name';
+  public constructor(private authService: AuthService) {}
+
+  public name = this.authService.username || 'Your name';
 }
