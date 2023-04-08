16a5542af37dd4e76a28afab63fceaf16f4e47e5
Ponomareva Nina <nina.9412@mail.ru>
2023-04-08 14:02:07 +0300

    feat: add login observer

--- youtube_client_app/src/app/core/header/profile/profile.component.ts
+++ youtube_client_app/src/app/core/header/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { AUTH_PAGE_ROUTE } from '../../../../constants/routing-constants';
 
@@ -13,6 +14,10 @@ export default class ProfileComponent {
     protected authService: AuthService,
     private router: Router
   ) {}
+
+  public get authState(): Observable<boolean> {
+    return this.authService.checkLogin();
+  }
 
   public handleLoginClick(): void {
     this.router.navigate([AUTH_PAGE_ROUTE]);
